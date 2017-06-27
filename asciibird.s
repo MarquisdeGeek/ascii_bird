9d37ed57b893959134976982a786fc31202a9ed8
Steven Goodwin <sgoodwin@brightsign.biz>
2017-06-27 10:09:06 +0100

    Remove TODO notes from primary source file

--- asciibird.s
+++ asciibird.s
@@ -18,33 +18,6 @@
 	INCLUDE "game/logic.s"
 	INCLUDE "game/main.s"
 
-
-; http://www.6809.org.uk/dragon/6809e.txt
-; http://dragon32.info/info/memmap.txt
-; http://dragon32.info/info/romref.html
-; http://atjs.mbnet.fi/mc6809/Information/6809.htm
-
-
-
-; Check SGX names
-; Limit memory used by objects
-
-;38798 978E RANDOM NUMBER: Generates an 8 bit random         number and puts it in location 278
-
-; Second pipe to not erase background. HOW?W?
-
-; Game loop to have 16 (?) slots for game objects. 
-; check @TODO
-
-; Proper key handling
-
-
-; Random height of pipes
-
-; OPT:V2: Create town:
-  ; Generate data block on start
-  ; Randomize colours (MSB = set it and select random colour, else just set MSB)
-
 game_start
 
 	jsr sgx.graphics.init
